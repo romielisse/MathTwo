ccfd29aab114a1e8454ea646da6bd17091780f29
Melissa Fung <romielisse@gmail.com>
2018-05-31 12:50:36 -0400

    Initial commit

---
+++ src/TwoNumbers.java
@@ -0,0 +1,10 @@
+public class TwoNumbers {
+    public static void main(String[] args){
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+        System.out.println();
+    }
+}
